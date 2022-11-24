5a8d1e880b47329f5c99f9e310e2f25bd2855437
Jackson Gravino <103889584+JacksonJLGravino@users.noreply.github.com>
2022-11-24 15:19:27 -0300

    lista de supermercado 2.0

---
+++ README.md
@@ -0,0 +1,16 @@
+# Lista de Supermercado
+
+Site para fazer lista de supermercado e gerar um pdf com a lista
+
+<img src="capa.jpg" />
+
+___
+
+## Tecnologias 💻
+
+- HTML
+- CSS
+- JAVASCRIPT
+- [html2pdf.js](https://ekoopmans.github.io/html2pdf.js/)
+
+___
--- main.js
+++ main.js
@@ -1,36 +1,107 @@
-var items = []
+const produto = document.querySelector('#produto')
+const valor = document.querySelector('#valor')
+const btnAddProduto = document.querySelector('#btnAddProduto')
+const listaProduto = document.querySelector('#listaProduto')
+const tudoPDF = document.querySelector('#lista')
 
-document.querySelector('input[type=submit]').addEventListener('click', () => {
-  var nomeProduto = document.querySelector('input[name=nome_produto]')
-  var precoProduto = document.querySelector('input[name=price]')
+let valorTotal = 0
 
-  items.push({
-    nome: nomeProduto.value,
-    valor: precoProduto.value
-  })
+btnAddProduto.addEventListener('click', e => {
+  let total = 0
+  let novoProduto = {
+    nome: produto.value,
+    preco: valor.value,
+    id: gerarId()
+  }
 
-  let listaProdutos = document.querySelector('.lista-produtos')
-  let soma = 0
-  listaProdutos.innerHTML = ''
-  items.map(function (val) {
-    soma += parseFloat(val.valor)
-    listaProdutos.innerHTML += `
-    <div class="lista-produto-single">
-      <h3>${val.nome}</h3>
-      <h3 class="price-produto"><span>R$${val.valor}</span></h3>
-    </div>`
-  })
-  soma = soma.toFixed(2)
-  nomeProduto.value = ''
-  precoProduto.value = ''
-
-  let elementoSoma = document.querySelector('.soma-produto h1')
-  elementoSoma.innerHTML = 'Total: R$' + soma
+  adicionarProduto(novoProduto)
+  somarValor(novoProduto)
 })
 
-document.querySelector('button').addEventListener('click', () => {
-  items = []
+function gerarId() {
+  return Math.floor(Math.random() * 3000)
+}
 
-  document.querySelector('.lista-produtos').innerHTML = ''
-  document.querySelector('.soma-produto h1').innerHTML = 'Total: R$0'
-})
+function adicionarProduto(novoProduto) {
+  let li = criarTagLi(novoProduto)
+  listaProduto.appendChild(li)
+  produto.value = ''
+  valor.value = ''
+}
+
+function criarTagLi(novoProduto) {
+  let li = document.createElement('li')
+  li.id = novoProduto.id
+
+  let div = document.createElement('div')
+  div.classList.add('produto-info')
+
+  let pNome = document.createElement('p')
+  pNome.classList.add('nome-produto')
+  pNome.innerHTML = novoProduto.nome
+
+  let pValor = document.createElement('p')
+  pValor.classList.add('valor-produto')
+  pValor.innerHTML = 'R$ ' + novoProduto.preco
+
+  let button = document.createElement('button')
+  button.classList.add('btn-empt')
+  button.innerHTML = '<img src="./src/x.svg" alt="" />'
+  button.setAttribute(
+    'onclick',
+    'excluir(' + novoProduto.id + ',' + novoProduto.preco + ')'
+  )
+
+  div.appendChild(pNome)
+  div.appendChild(pValor)
+
+  li.appendChild(div)
+  li.appendChild(button)
+
+  return li
+}
+
+function excluir(idProduto, precoProduto) {
+  let confirmacao = window.confirm('Tem certeza que deseja excluir?')
+  if (confirmacao) {
+    let li = document.getElementById('' + idProduto + '')
+    if (li) {
+      listaProduto.removeChild(li)
+    }
+    let valorProduto = parseFloat(precoProduto)
+    valorTotal -= valorProduto
+    criarTotal(valorTotal)
+  }
+}
+
+function somarValor(novoProduto) {
+  let valorProduto = parseFloat(novoProduto.preco)
+  if (valorProduto > 0) {
+    valorTotal += valorProduto
+  }
+  criarTotal(valorTotal)
+}
+
+function criarTotal(valorProduto) {
+  let h2 = document.querySelector('#total')
+  h2.innerHTML = 'Total R$' + valorProduto
+}
+
+function deletarTudo() {
+  let confirmacao = window.confirm('Tem certeza que deseja excluir tudo?')
+  if (confirmacao) {
+    listaProduto.innerHTML = ''
+    criarTotal(0)
+  }
+}
+
+function baixar() {
+  var opt = {
+    margin: 1,
+    filename: 'lista.pdf',
+    image: { type: 'jpeg', quality: 0.98 },
+    html2canvas: { scale: 2 },
+    jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+  }
+  html2pdf().set(opt).from(tudoPDF).save()
+}
